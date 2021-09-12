0b92b73a86b64eace768eedabd85696969a2c70e
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-09-13 02:07:17 +0300

    часть 2 готова

--- part2/calc.go
+++ part2/calc.go
@@ -17,40 +17,33 @@ func isNumber(number string) bool {
 	return rNumber.MatchString(number)
 }
 func isSign(sign string) bool {
-	rSign := regexp.MustCompile(`[+\-*/()]`)
+	rSign := regexp.MustCompile(`[+\-*/]`)
 	return rSign.MatchString(sign)
 }
 
-func parseStr(expressionStr string) (expression []string, err error) {
-	rSpace := regexp.MustCompile(`\s+`)
-	inputStrWithoutSpace := rSpace.ReplaceAllString(expressionStr, "")
-
-	var number string
-	for _, symbol := range strings.Split(inputStrWithoutSpace, "") {
-		if isNumber(symbol) {
-			number += symbol
-		} else if isSign(symbol) {
-			if number != "" && number != "-" {
-				expression = append(expression, number, symbol)
-				number = ""
-			} else if symbol == "-" {
-				number = "-"
-			} else {
-				expression = append(expression, symbol)
-			}
+// Из массива символов, начиная с позиции startPos, составляем число пока
+// нам встречаются цифры. Возвращаем само число и позицию endPos, на которой оно закончилось.
+func getNumberFromString(charArray []string, startPos int) (number string, endPos int) {
+	for endPos = startPos; endPos < len(charArray); endPos++ {
+		if isNumber(charArray[endPos]) {
+			number += charArray[endPos]
 		} else {
-			return nil, fmt.Errorf("invalid character: %s", symbol)
+			endPos--
+			return
 		}
-
-	}
-	if number != "" {
-		expression = append(expression, number)
 	}
 
 	return
 }
 
+// Переводим входную строку в обратную польскую запись.
+// В случае невалидного ввода, вовращаем ошибки
 func convertExpToRPN(expressionStr string) (rpn []string, err error) {
+	rSpace := regexp.MustCompile(`\s+`)
+	expressionWithoutSpace := rSpace.ReplaceAllString(expressionStr, "")
+
+	expression := strings.Split(expressionWithoutSpace, "")
+
 	priorityOfOperations := map[string]uint{
 		"(": 0,
 		")": 1,
@@ -59,20 +52,23 @@ func convertExpToRPN(expressionStr string) (rpn []string, err error) {
 		"*": 4,
 		"/": 4,
 	}
-
 	var stk stack.Stack
+	sign := ""
 
-	expression, err := parseStr(expressionStr)
-	if err != nil {
-		return nil, fmt.Errorf("parsing an expression: %s", err)
-	}
+	for i := 0; i < len(expression); i++ {
+		if expression[i] == "-" && (i == 0 || expression[i-1] == "(") {
+			sign = "-"
+		} else if isNumber(expression[i]) {
+			number, endPos := getNumberFromString(expression, i)
+			i = endPos
 
-	for _, currLexeme := range expression {
-		if isNumber(currLexeme) {
-			rpn = append(rpn, currLexeme)
-		} else if currLexeme == "(" {
-			stk.Push(currLexeme)
-		} else if currLexeme == ")" {
+			number = sign + number
+			sign = ""
+
+			rpn = append(rpn, number)
+		} else if expression[i] == "(" {
+			stk.Push(expression[i])
+		} else if expression[i] == ")" {
 			topStack, err := stk.Pop()
 			if err != nil {
 				return nil, fmt.Errorf("missing opening parenthesis: %s", err)
@@ -84,13 +80,13 @@ func convertExpToRPN(expressionStr string) (rpn []string, err error) {
 					return nil, fmt.Errorf("missing opening parenthesis: %s", err)
 				}
 			}
-		} else {
+		} else if isSign(expression[i]) {
 			if !stk.IsEmpty() {
 				topStack, err := stk.Top()
 				if err != nil {
 					return nil, err
 				}
-				if priorityOfOperations[topStack] >= priorityOfOperations[currLexeme] {
+				if priorityOfOperations[topStack] >= priorityOfOperations[expression[i]] {
 					rpn = append(rpn, topStack)
 					_, err = stk.Pop()
 					if err != nil {
@@ -98,7 +94,9 @@ func convertExpToRPN(expressionStr string) (rpn []string, err error) {
 					}
 				}
 			}
-			stk.Push(currLexeme)
+			stk.Push(expression[i])
+		} else {
+			return nil, fmt.Errorf("invalid character: %s", expression[i])
 		}
 	}
 
@@ -113,6 +111,8 @@ func convertExpToRPN(expressionStr string) (rpn []string, err error) {
 	return
 }
 
+// Выполняет математическую операцию над аргументами типа String,
+// возвращяя результат вычисления тип String
 func mathOperationsOnStrings(leftArgumentStr, sign, rightArgumentStr string) (resultStr string, err error) {
 	leftArgument, err := strconv.Atoi(leftArgumentStr)
 	if err != nil {
@@ -138,6 +138,7 @@ func mathOperationsOnStrings(leftArgumentStr, sign, rightArgumentStr string) (re
 	return strconv.Itoa(result), nil
 }
 
+// Получает на вход обратную польскую запись и вычисляет ее
 func calcRPN(rpn []string) (int, error) {
 	var stk stack.Stack
 
@@ -209,9 +210,3 @@ func main() {
 
 	fmt.Println(result)
 }
-
-// TODO
-// 3. Тесты с ошибками
-// 4. Рефакторинг
-// 5. Чек вк
-// 6. Пуш
--- part2/calc_test.go
+++ part2/calc_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -22,6 +21,9 @@ func TestCalc(t *testing.T) {
 		{"Скобки 1", "10 + (2 - 9) * 2", -4, require.Equal},
 		{"Скобки 2", "(2 - 9) * (2 + 10)", -84, require.Equal},
 		{"Скобки 3", "3 + 4 * 2 / (1 - 5)*2", -1, require.Equal},
+		{"Скобки 4", "(-3) + (-10)", -13, require.Equal},
+		{"Скобки 5", "(-3) - (-10)", 7, require.Equal},
+		{"Скобки 6", "(-3) * (-10)", 30, require.Equal},
 		{"Вложенные скобки 1", "((10 + 2 * (2 - 9)) * 2)", -8, require.Equal},
 		{"Вложенные скобки 2", "((1+2*3))*((2-1)/1)", 7, require.Equal},
 		{`Скобки и знак минус 1`, "5 * (-2)", -10, require.Equal},
@@ -60,35 +62,12 @@ func TestCalcWrongExpression(t *testing.T) {
 		{"Незакрытые скобки 5", "3 - )4+2)*6", 0, require.Equal},
 		{"Пустые скобки 1", "()", 0, require.Equal},
 		{"Пустые скобки 2", "-()", 0, require.Equal},
-		{"Унарный оператор", "-(2)", 0, require.Equal},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			result, _ := calc(tt.inputStr)
 			tt.assertion(t, tt.expect, result)
-		})
-	}
-}
-
-func TestConvert(t *testing.T) {
-	tests := []struct {
-		name      string
-		inputStr  string
-		expect    []string
-		assertion require.ComparisonAssertionFunc
-	}{
-		{"", "1 + 2", []string{"1", "2", "+"}, require.Equal},
-		{"", "1 + 2    + 3   ", []string{"1", "2", "+", "3", "+"}, require.Equal},
-		{"", "15 + 200    + 3   ", []string{"15", "200", "+", "3", "+"}, require.Equal},
-		{"", "3 + 4 * 2 / (1 - 5)*2", strings.Split("342*15-/2*+", ""), require.Equal},
-		{"", "((1+2*3))*((2-1)/1)", strings.Split("123*+21-1/*", ""), require.Equal},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c, _ := convertExpToRPN(tt.inputStr)
-			tt.assertion(t, tt.expect, c)
 		})
 	}
 }
