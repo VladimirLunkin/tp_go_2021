c587d41f1e3cfb3ead6bee3c9fb9a2428810d05b
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-09-20 02:49:31 +0300

    тесты прошли

--- hw_2/signer/signer.go
+++ hw_2/signer/signer.go
@@ -11,16 +11,15 @@ import (
 func ExecutePipeline(jobs ...job) {
 	wg := &sync.WaitGroup{}
 	in := make(chan interface{}, 1)
-	for i, j := range jobs {
+	for _, j := range jobs {
 		wg.Add(1)
 		out := make(chan interface{}, 1)
 		go startWorker(j, in, out, wg)
-		if i == 0 {
-			in <- ""
-		}
 		in = out
 	}
-	fmt.Println(<-in)
+	for ch := range in {
+		fmt.Println(ch)
+	}
 	wg.Wait()
 }
 
@@ -30,57 +29,78 @@ func startWorker(jobFunc job, in, out chan interface{}, waiter *sync.WaitGroup) 
 
 	jobFunc(in, out)
 }
-
-func SingleHash(in, out chan interface{}) {
-	for ch := range in {
-		data := strconv.Itoa(ch.(int))
-
-		DataSignerOut1 := make(chan string)
-		go DataSignerWorker(DataSignerCrc32, data, DataSignerOut1)
-
-		DataSignerOut2 := make(chan string)
-		go DataSignerWorker(DataSignerMd5, data, DataSignerOut2)
-		s2 := <-DataSignerOut2
-
-		DataSignerOut3 := make(chan string)
-		go DataSignerWorker(DataSignerCrc32, s2, DataSignerOut3)
-
-		s1 := <-DataSignerOut1
-		s3 := <-DataSignerOut3
-
-		out <- s1 + "~" + s3
-	}
-}
-
 type DataSigner func(string2 string) string
-
 func DataSignerWorker(dataSigner DataSigner, data string, out chan string) {
 	out <- dataSigner(data)
 }
-
+func md5Worker( data string, out chan string) {
+	out <- DataSignerMd5(data)
+}
 const kMultiHash int = 6
 
+func SingleHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
+	mu := sync.Mutex{}
+	for ch := range in {
+		data := strconv.Itoa(ch.(int))
+
+		wg.Add(1)
+
+		go func(out chan interface{}, data string, waiter *sync.WaitGroup) {
+			defer wg.Done()
+
+			DataSignerOut1 := make(chan string)
+			go DataSignerWorker(DataSignerCrc32, data, DataSignerOut1)
+
+			mu.Lock()
+			DataSignerOut2 := make(chan string)
+			go DataSignerWorker(DataSignerMd5, data, DataSignerOut2)
+			s2 := <-DataSignerOut2
+			mu.Unlock()
+
+			DataSignerOut3 := make(chan string)
+			go DataSignerWorker(DataSignerCrc32, s2, DataSignerOut3)
+
+			s1 := <-DataSignerOut1
+			s3 := <-DataSignerOut3
+
+			out <- s1 + "~" + s3
+		}(out, data, wg)
+	}
+
+	wg.Wait()
+}
+
 func MultiHash(in, out chan interface{}) {
+	wg := &sync.WaitGroup{}
 	for ch := range in {
 		data := ch.(string)
 
-		var chs [kMultiHash]chan string
-		for i := range chs {
-			chs[i] = make(chan string)
-		}
+		wg.Add(1)
 
-		for th := 0; th < kMultiHash; th++ {
-			go DataSignerWorker(DataSignerCrc32, strconv.Itoa(th)+data, chs[th])
-		}
+		go func(data string, waiter *sync.WaitGroup) {
+			defer waiter.Done()
 
-		var result string
+			var chs [kMultiHash]chan string
+			for i := range chs {
+				chs[i] = make(chan string)
+			}
 
-		for i := range chs {
-			result += <-chs[i]
-		}
+			for th := 0; th < kMultiHash; th++ {
+				go DataSignerWorker(DataSignerCrc32, strconv.Itoa(th)+data, chs[th])
+			}
 
-		out <- result
+			var result string
+
+			for i := range chs {
+				result += <-chs[i]
+			}
+
+			out <- result
+		}(data, wg)
 	}
+
+	wg.Wait()
 }
 
 func CombineResults(in, out chan interface{}) {
@@ -101,8 +121,10 @@ func CombineResults(in, out chan interface{}) {
 }
 
 func qwe(in, out chan interface{}) {
-	out <- 0
-	out <- 1
+	for _, fibNum := range []int{0, 1, 1, 2, 3, 5, 8} {
+	//for _, fibNum := range []string{"0", "1", "1", "2", "3", "5", "8"} {
+		out <- fibNum
+	}
 }
 
 func main() {
