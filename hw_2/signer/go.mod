5a17be672c389b718e470f1e75171878e3fe77d2
Vladimir Lunkin <vova.gold.201427@gmail.com>
2021-09-20 01:35:03 +0300

    функции хэширования

---
+++ hw_2/signer/go.mod
@@ -0,0 +1,3 @@
+module github.com/VladimirLunkin/tp_go_2021/hw_2/signer
+
+go 1.17
--- hw_2/signer/signer.go
+++ hw_2/signer/signer.go
@@ -2,44 +2,112 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"strings"
+	"sort"
+	"strconv"
 	"sync"
 	"time"
 )
 
-const (
-	iterationsNum = 7
-	goroutinesNum = 5
-)
-
-func startWorker(in int, waiter *sync.WaitGroup) {
-	defer waiter.Done() // wait_2.go уменьшаем счетчик на 1
-	localWg := &sync.WaitGroup{}
-	for j := 0; j < iterationsNum; j++ {
-		fmt.Printf(formatWork(in, j))
-		time.Sleep(time.Millisecond) // попробуйте убрать этот sleep
-	}
-
-	localWg.Wait()
-}
-
 func ExecutePipeline(jobs ...job) {
 	wg := &sync.WaitGroup{}
-	for _, j := range jobs {
+	in := make(chan interface{}, 1)
+	for i, j := range jobs {
 		wg.Add(1)
-		go j(1, 2)
+		out := make(chan interface{}, 1)
+		go startWorker(j, in, out, wg)
+		if i == 0 {
+			in <- ""
+		}
+		in = out
 	}
+	fmt.Println(<-in)
 	wg.Wait()
 }
 
-func formatWork(in, j int) string {
-	return fmt.Sprintln(strings.Repeat("  ", in), "█",
-		strings.Repeat("  ", goroutinesNum-in),
-		"th", in,
-		"iter", j, strings.Repeat("■", j))
+func startWorker(jobFunc job, in, out chan interface{}, waiter *sync.WaitGroup) {
+	defer waiter.Done()
+	defer close(out)
+
+	jobFunc(in, out)
+}
+
+func SingleHash(in, out chan interface{}) {
+	for ch := range in {
+		data := strconv.Itoa(ch.(int))
+
+		DataSignerOut1 := make(chan string)
+		go DataSignerWorker(DataSignerCrc32, data, DataSignerOut1)
+
+		DataSignerOut2 := make(chan string)
+		go DataSignerWorker(DataSignerMd5, data, DataSignerOut2)
+		s2 := <-DataSignerOut2
+
+		DataSignerOut3 := make(chan string)
+		go DataSignerWorker(DataSignerCrc32, s2, DataSignerOut3)
+
+		s1 := <-DataSignerOut1
+		s3 := <-DataSignerOut3
+
+		out <- s1 + "~" + s3
+	}
+}
+
+type DataSigner func(string2 string) string
+
+func DataSignerWorker(dataSigner DataSigner, data string, out chan string) {
+	out <- dataSigner(data)
+}
+
+const kMultiHash int = 6
+
+func MultiHash(in, out chan interface{}) {
+	for ch := range in {
+		data := ch.(string)
+
+		var chs [kMultiHash]chan string
+		for i := range chs {
+			chs[i] = make(chan string)
+		}
+
+		for th := 0; th < kMultiHash; th++ {
+			go DataSignerWorker(DataSignerCrc32, strconv.Itoa(th)+data, chs[th])
+		}
+
+		var result string
+
+		for i := range chs {
+			result += <-chs[i]
+		}
+
+		out <- result
+	}
+}
+
+func CombineResults(in, out chan interface{}) {
+	var results []string
+	for ch := range in {
+		results = append(results, ch.(string))
+	}
+
+	sort.Strings(results)
+
+	var res string
+
+	for _, str := range results {
+		res += str + "_"
+	}
+
+	out <- res[:len(res)-1]
+}
+
+func qwe(in, out chan interface{}) {
+	out <- 0
+	out <- 1
 }
 
 func main() {
-	ExecutePipeline()
+	tStart := time.Now()
+	ExecutePipeline(qwe, SingleHash, MultiHash, CombineResults)
+	tFinish := time.Now()
+	fmt.Println(tFinish.UnixMicro() - tStart.UnixMicro())
 }
